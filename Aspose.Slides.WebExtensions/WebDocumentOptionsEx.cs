f604b3fe1cbe4c47d92c27f9b9729075a3691ce4
Alexander Mosyagin <alexander.mosyagin@aspose.com>
2023-09-26 13:07:36 +0300

    SLIDESNET-43218: Implemented notes export

---
+++ Aspose.Slides.WebExtensions/WebDocumentOptionsEx.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2001-2020 Aspose Pty Ltd. All Rights Reserved.
+
+using Aspose.Slides.Export.Web;
+using Aspose.Slides.Export;
+
+namespace Aspose.Slides.WebExtensions
+{
+    public class WebDocumentOptionsEx : WebDocumentOptions 
+    { 
+        public NotesCommentsLayoutingOptions NotesCommentsLayoutingOptions { get; set; } = new NotesCommentsLayoutingOptions();
+    }
+}
