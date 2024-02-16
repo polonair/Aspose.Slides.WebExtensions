08f838f282ea281687ab8441325643a8bffc6a99
Alexander Mosyagin <alexander.mosyagin@aspose.com>
2024-02-17 00:28:07 +0300

    SLIDESNET-44379: cashing implemented

--- Aspose.Slides.WebExtensions/RazorEngineService.cs
+++ Aspose.Slides.WebExtensions/RazorEngineService.cs
@@ -6,6 +6,7 @@ using RazorEngineCore;
 using System.Linq;
 using System.Drawing;
 using Aspose.Slides.Export.Web;
+using System.Threading;
 
 namespace Aspose.Slides.WebExtensions
 {
@@ -13,6 +14,7 @@ namespace Aspose.Slides.WebExtensions
     {
         IRazorEngine razorEngine;
         IDictionary<string, string> parts = new Dictionary<string, string>();
+        IDictionary<string, IRazorEngineCompiledTemplate<TemplateBase>> cash = new Dictionary<string, IRazorEngineCompiledTemplate<TemplateBase>>();
         internal static IRazorEngineService Create(TemplateServiceConfiguration config)
         {
             return new RazorEngineService();
@@ -33,72 +35,37 @@ namespace Aspose.Slides.WebExtensions
         {
             //throw new NotImplementedException();
         }
+        IRazorEngineCompiledTemplate<TemplateBase> CashCompile(string content)
+        {
+            IRazorEngineCompiledTemplate<TemplateBase> cmp = null;
+            if (cash.TryGetValue(content, out cmp)) return cmp;
+            return cash[content] = razorEngine.Compile<TemplateBase>(content, builder =>
+            {
+                builder.AddUsing("System");
+                builder.AddUsing("System.Drawing");
+                builder.AddUsing("System.Collections");
+                builder.AddUsing("System.Collections.Generic");
+                builder.AddUsing("Aspose.Slides");
+                builder.AddUsing("Aspose.Slides.Export.Web");
+                builder.AddUsing("Aspose.Slides.WebExtensions.Helpers");
 
+                builder.AddAssemblyReference(typeof(Presentation));
+                builder.AddAssemblyReference(typeof(SizeF));
+                builder.AddAssemblyReference(typeof(FontStyle));
+                builder.AddAssemblyReference(typeof(Dictionary<,>));
+                builder.AddAssemblyReference(typeof(TemplateContext<>));
+                try { builder.AddAssemblyReferenceByName("System.Collections"); }
+                catch { }
+                builder.AddAssemblyReferenceByName("System.Drawing.Common");
+                builder.AddAssemblyReference(GetType());
+                //builder.IncludeDebuggingInfo();
+            });
+        }
         public string Run(string key, Type type, object model)
         {
-            var t =  new CompiledTemplate(
-                    razorEngine.Compile<TemplateBase>(parts[key], builder =>
-                    {
-                        //builder.Options.DefaultUsings.Add("System");
-                        //builder.Options.DefaultUsings.Add("System.Drawing");
-                        //builder.Options.DefaultUsings.Add("System.Collections");
-                        //builder.Options.DefaultUsings.Add("System.Collections.Generic");
-                        ////builder.Options.DefaultUsings.Add("System.Drawing.Imaging");
-                        //builder.Options.DefaultUsings.Add("Aspose.Slides");
-                        //builder.Options.DefaultUsings.Add("Aspose.Slides.Export.Web");
-                        //builder.Options.DefaultUsings.Add("Aspose.Slides.WebExtensions.Helpers");
-
-                        builder.AddUsing("System");
-                        builder.AddUsing("System.Drawing");
-                        builder.AddUsing("System.Collections");
-                        builder.AddUsing("System.Collections.Generic");
-                        builder.AddUsing("Aspose.Slides");
-                        builder.AddUsing("Aspose.Slides.Export.Web");
-                        builder.AddUsing("Aspose.Slides.WebExtensions.Helpers");
-                        
-                        builder.AddAssemblyReference(typeof(Presentation));
-                        builder.AddAssemblyReference(typeof(SizeF));
-                        builder.AddAssemblyReference(typeof(FontStyle));
-                        builder.AddAssemblyReference(typeof(Dictionary<,>));
-                        builder.AddAssemblyReference(typeof(TemplateContext<>));
-                        try { builder.AddAssemblyReferenceByName("System.Collections"); }
-                        catch { }
-                        builder.AddAssemblyReferenceByName("System.Drawing.Common");
-                        builder.AddAssemblyReference(GetType());
-                        //builder.IncludeDebuggingInfo();
-                    }),
-                    parts.ToDictionary(
-                            k => k.Key,
-                            v => razorEngine.Compile<TemplateBase>(v.Value, builder =>
-                            {
-                                //builder.Options.DefaultUsings.Add("System");
-                                //builder.Options.DefaultUsings.Add("System.Drawing");
-                                //builder.Options.DefaultUsings.Add("System.Collections");
-                                //builder.Options.DefaultUsings.Add("System.Collections.Generic");
-                                ////builder.Options.DefaultUsings.Add("System.Drawing.Imaging");
-                                //builder.Options.DefaultUsings.Add("Aspose.Slides");
-                                //builder.Options.DefaultUsings.Add("Aspose.Slides.Export.Web");
-                                //builder.Options.DefaultUsings.Add("Aspose.Slides.WebExtensions.Helpers");
-
-                                builder.AddUsing("System");
-                                builder.AddUsing("System.Drawing");
-                                builder.AddUsing("System.Collections");
-                                builder.AddUsing("System.Collections.Generic");
-                                builder.AddUsing("Aspose.Slides");
-                                builder.AddUsing("Aspose.Slides.Export.Web");
-                                builder.AddUsing("Aspose.Slides.WebExtensions.Helpers");
-
-                                builder.AddAssemblyReference(typeof(Presentation));
-                                builder.AddAssemblyReference(typeof(SizeF));
-                                builder.AddAssemblyReference(typeof(FontStyle));
-                                builder.AddAssemblyReference(typeof(Dictionary<,>));
-                                builder.AddAssemblyReference(typeof(TemplateContext<>));
-                                try { builder.AddAssemblyReferenceByName("System.Collections"); }
-                                catch { }
-                                builder.AddAssemblyReferenceByName("System.Drawing.Common");
-                                builder.AddAssemblyReference(GetType()); 
-                                //builder.IncludeDebuggingInfo();
-                            })));
+            var t = new CompiledTemplate(
+                CashCompile(parts[key]),
+                parts.ToDictionary(k => k.Key, v => CashCompile(v.Value)));
 
             return t.Run(model);
         }
