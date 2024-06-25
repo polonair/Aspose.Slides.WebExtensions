a220727a44b486880cad6cabae3dc4f4ce24ba9b
Alexander Mosyagin <alexander.mosyagin@aspose.com>
2024-06-25 15:43:17 +0300

    SLIDESNET-43219: right-sided comments implemented for single-page case

--- Aspose.Slides.WebExtensions.Tests/SLIDESNET_43219.cs
+++ Aspose.Slides.WebExtensions.Tests/SLIDESNET_43219.cs
@@ -42,7 +42,41 @@ namespace Aspose.Slides.WebExtensions.Tests
 
             CompareDir(EthalonPath, OutputPath, _ => _);
         }
-        
+
+        [TestMethod]
+        public void Test_43219_Right()
+        {
+            var RootDirectory = Path.GetFullPath("../../../");
+            var PresentationFilePath = Path.Combine(RootDirectory, "TestData", "SLIDESNET_43219", "SLIDESNET-43219.pptx");
+            var EthalonPath = Path.Combine(RootDirectory, "TestData", "SLIDESNET_43219", "html_right");
+            var TemplatePath = Path.Combine(RootDirectory, "TestData", "Out", "templates");
+            var OutputPath = Path.Combine(RootDirectory, "TestData", "Out", "SLIDESNET_43219", "html_right");
+
+            var sourcePath1 = Path.GetFullPath(Path.Combine(RootDirectory, "..", "Aspose.Slides.WebExtensions", "templates", "single-page"));
+            var sourcePath2 = Path.GetFullPath(Path.Combine(RootDirectory, "..", "Aspose.Slides.WebExtensions", "templates", "common"));
+
+            Directory.CreateDirectory(OutputPath);
+            Directory.CreateDirectory(TemplatePath);
+
+            foreach (string dirPath in Directory.GetDirectories(sourcePath1, "*", SearchOption.AllDirectories)) Directory.CreateDirectory(dirPath.Replace(sourcePath1, TemplatePath));
+            foreach (string newPath in Directory.GetFiles(sourcePath1, "*.*", SearchOption.AllDirectories)) File.Copy(newPath, newPath.Replace(sourcePath1, TemplatePath), true);
+
+            foreach (string dirPath in Directory.GetDirectories(sourcePath2, "*", SearchOption.AllDirectories)) Directory.CreateDirectory(dirPath.Replace(sourcePath2, TemplatePath));
+            foreach (string newPath in Directory.GetFiles(sourcePath2, "*.*", SearchOption.AllDirectories)) File.Copy(newPath, newPath.Replace(sourcePath2, TemplatePath), true);
+
+            using (Presentation pres = new Presentation(PresentationFilePath))
+            {
+                WebDocument document = pres.ToSinglePageWebDocument(
+                    new WebDocumentOptions { EmbedImages = true, },
+                    TemplatePath,
+                    OutputPath,
+                    new NotesCommentsLayoutingOptions() { CommentsPosition = CommentsPositions.Right });
+                document.Save();
+            }
+
+            CompareDir(EthalonPath, OutputPath, _ => _);
+        }
+
         private void CompareDir(string ethalonPath, string outputPath, CustomReplacement replacements)
         {
             int cnt = 0;
